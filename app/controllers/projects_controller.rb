b937e8ae198308eb77cb9792ab30b5704cffe19b
bbryant7 <bailey.bryant10@gmail.com>
2020-10-06 22:14:01 -0500

    add ability to create and display projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.where(user_id: current_user.id).order(updated_at: :desc)
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    project = Project.new(project_params)
+    project.user_id = current_user.id
+    if project.save
+      redirect_to projects_path
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
+
 end
